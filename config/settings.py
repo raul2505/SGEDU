6f75b5b8a509ff42bd03eae0868626e1627dd2b8
Erick Jauregui <erick25052004@gami.com>
2025-04-18 15:28:35 -0500

    Configuracion de la Base de datos

--- config/settings.py
+++ config/settings.py
@@ -72,14 +72,19 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#Conexion a la base de datos POSTGRESQL
+from decouple import config
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
